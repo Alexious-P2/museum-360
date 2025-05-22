85edeb3aca8e9761536ed8a5c5835a606aed2eb7
Alexious-P2 <alexious.fernando@position2.com>
2025-05-22 15:02:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,319 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-centre",
+      "name": "Centre",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.0018918978760797955,
+        "pitch": 0.010392316135012791,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.013000175706384454,
+          "pitch": 0.35395890617106573,
+          "rotation": 0,
+          "target": "4-sculptures"
+        },
+        {
+          "yaw": -1.5686267113768864,
+          "pitch": 0.323427905486378,
+          "rotation": 0,
+          "target": "3-paintings"
+        },
+        {
+          "yaw": -3.1338003967541397,
+          "pitch": 0.3447294825366569,
+          "rotation": 0,
+          "target": "1-war-memorial-statue"
+        },
+        {
+          "yaw": 1.5869592442286713,
+          "pitch": 0.3469405926756153,
+          "rotation": 0,
+          "target": "2-balloon-sculpt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-war-memorial-statue",
+      "name": "War Memorial Statue",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.102571474329501,
+          "pitch": 0.2888969152863545,
+          "rotation": 6.283185307179586,
+          "target": "2-balloon-sculpt"
+        },
+        {
+          "yaw": -3.120421592627615,
+          "pitch": 0.38093020533390565,
+          "rotation": 0,
+          "target": "0-centre"
+        },
+        {
+          "yaw": -3.1332916086849476,
+          "pitch": 0.1720952382456442,
+          "rotation": 0,
+          "target": "4-sculptures"
+        },
+        {
+          "yaw": 2.0416807217608373,
+          "pitch": 0.25877959964547514,
+          "rotation": 0,
+          "target": "3-paintings"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004268018770311244,
+          "pitch": 0.13715223759423267,
+          "title": "<font color=\"#2b2b2b\" face=\"Lato, Lucida, Verdana, Arial, Helvetica, sans-serif\"><span style=\"white-space-collapse: preserve-breaks;\"><b><i>Députés morts pour la France</i></b></span></font>",
+          "text": "<div>Victory of Law</div><div>1916 - 1919</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-balloon-sculpt",
+      "name": "Balloon Sculpt",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1437070513347685,
+          "pitch": 0.2526688680538616,
+          "rotation": 6.283185307179586,
+          "target": "4-sculptures"
+        },
+        {
+          "yaw": 3.133507401066396,
+          "pitch": 0.05873219862154144,
+          "rotation": 0,
+          "target": "3-paintings"
+        },
+        {
+          "yaw": -3.119214644528796,
+          "pitch": 0.4442460634821117,
+          "rotation": 0,
+          "target": "0-centre"
+        },
+        {
+          "yaw": 2.171912501981197,
+          "pitch": 0.29035859170985745,
+          "rotation": 12.566370614359176,
+          "target": "1-war-memorial-statue"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.24793854699774087,
+          "pitch": 0.0858940813839908,
+          "title": "Baloon Sculpture",
+          "text": "<font color=\"#202122\" face=\"sans-serif\"><span style=\"font-size: 16px;\"><b>Jeffrey Lynn Koons is an American artist recognized for his work dealing with popular culture and his sculptures depicting everyday objects, including balloon animals produced in stainless steel with mirror-finish surfaces.</b></span></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-paintings",
+      "name": "Paintings",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.1383428955552066,
+        "pitch": 0.012602519450458871,
+        "fov": 1.0401733393295245
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.0094061556173273,
+          "pitch": 0.2680387181829822,
+          "rotation": 0,
+          "target": "1-war-memorial-statue"
+        },
+        {
+          "yaw": -0.03029817624104325,
+          "pitch": 0.16725000263900647,
+          "rotation": 0,
+          "target": "2-balloon-sculpt"
+        },
+        {
+          "yaw": -0.01584969725894858,
+          "pitch": 0.40662060557943924,
+          "rotation": 0,
+          "target": "0-centre"
+        },
+        {
+          "yaw": -1.0160334043599608,
+          "pitch": 0.2628430208886918,
+          "rotation": 0,
+          "target": "4-sculptures"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.12387735558743,
+          "pitch": 0.10574153740556369,
+          "title": "Abstract Art",
+          "text": "<font color=\"#202122\" face=\"sans-serif\"><span style=\"font-size: 16px;\"><b>Abstract art uses visual language of shape, form, color and line to create a composition which may exist with a degree of independence from visual references in the world.</b></span></font>"
+        }
+      ]
+    },
+    {
+      "id": "4-sculptures",
+      "name": "Sculptures",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0024940052801767365,
+        "pitch": 0.1834615672452724,
+        "fov": 1.2090009407416336
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.0080943162371376,
+          "pitch": 0.10506132879618946,
+          "rotation": 0,
+          "target": "3-paintings"
+        },
+        {
+          "yaw": 3.141369128961296,
+          "pitch": 0.07413547490646621,
+          "rotation": 0,
+          "target": "1-war-memorial-statue"
+        },
+        {
+          "yaw": -3.1263858072307826,
+          "pitch": 0.35291548922561944,
+          "rotation": 0,
+          "target": "0-centre"
+        },
+        {
+          "yaw": 2.0788908192633455,
+          "pitch": 0.2742156409536367,
+          "rotation": 0,
+          "target": "2-balloon-sculpt"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.007865011774359942,
+          "pitch": 0.42720992083157405,
+          "title": "Sculptures",
+          "text": "Sculpture in stone survives far better than works of art in perishable materials, and often represents the majority of the surviving works from ancient cultures."
+        }
+      ]
+    }
+  ],
+  "name": "Museum360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
